c6bbf602412305d573a299c59fa6c8cc8eb1b8e8
s01oblak <sasoblak@gmail.com>
2013-07-30 16:16:01 +0200

    asd

--- js/common.js
+++ js/common.js
@@ -1,46 +1,33 @@
 // Wait for device API libraries to load
-    //
-    document.addEventListener("deviceready", onDeviceReady, false);
+//
+document.addEventListener("deviceready", onDeviceReady, false);
 
-    
+// device APIs are available
+//
+function onDeviceReady() {
+	//var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+	//db.transaction(queryDB, errorCB, successCB);
 
-    // Query the database
-    //
-    function queryDB(tx) {
-        tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
-    }
+	//  $("#test").width($(document).width() - 15).height($(document).height() - 10);
+	//  alert($(document).width() + "px  x  " + $(document).height() + "px");
 
-    // Query the success callback
-    //
-    function querySuccess(tx, results) {
-        var len = results.rows.length;
-        var strIzpis = "";
-        strIzpis = "DEMO table: " + len + " rows found.\n";
-        for (var i=0; i<len; i++){
-            strIzpis = strIzpis + "Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data + "\n";
-        }
-        alert(strIzpis);
-    }
+	$("#test").click(function() {
 
-    // Transaction error callback
-    //
-    function errorCB(err) {
-        console.log("Error processing SQL: "+err.code);
-    }
+		navigator.camera.getPicture(onSuccess, onFail, {
+			quality : 50,
+			destinationType : Camera.DestinationType.DATA_URL
+		});
 
-    // Transaction success callback
-    //
-    function successCB() {
-        var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-        db.transaction(queryDB, errorCB);
-    }
+	});
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        //var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-        //db.transaction(queryDB, errorCB, successCB);
-        
-        $("#test").width($(document).width() - 15).height($(document).height() - 10);
-        alert($(document).width() + "px  x  " + $(document).height() + "px");
-    }
+}
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+
+function onFail(message) {
+	alert('Failed because: ' + message);
+}
