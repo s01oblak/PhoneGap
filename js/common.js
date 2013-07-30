a3d566cf734f71e8cd72390d9de2b5e5dc58190c
s01oblak <sasoblak@gmail.com>
2013-07-30 23:38:33 +0200

    sdfsdf

--- js/common.js
+++ js/common.js
@@ -1,79 +1,82 @@
 var strWindowOrientation = "";
 var strImageOrientation = "";
 
+
 // Wait for device API libraries to load
 //
 document.addEventListener("deviceready", onDeviceReady, false);
-$.mobile.orientationChangeEnabled = false;
+
 // device APIs are available
 //
 function onDeviceReady() {
-	document.addEventListener("orientationchange", onOrientationChange, false);
-	$("#container").css("opacity","0");
-	//var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-	//db.transaction(queryDB, errorCB, successCB);
+	$("#container").css("opacity", "0");
+	
+	
 
-	//  $("#test").width($(document).width() - 15).height($(document).height() - 10);
-	//  alert($(document).width() + "px  x  " + $(document).height() + "px");
 
-	$("#test").click(function() {
+	$(window).on("orientationchange", function(event) {
+		alert("This device is in " + event.orientation + " mode!");
+	});
 
-		navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-    		destinationType: Camera.DestinationType.FILE_URI, 
-    		correctOrientation: true });
+	// FOTOAPARAT
+	$("#btnOpenCamera").click(function() {
 
+		navigator.camera.getPicture(onSuccess, onFail, {
+			quality : 50,
+			destinationType : Camera.DestinationType.FILE_URI
+		});
 
 	});
-	
-	
-	
-	if ($(document).width() > $(document).height()){
-		strWindowOrientation = "landscape";
-	}else{
-		strWindowOrientation = "portrait";		
-	}
-	
-	//alert(strWindowOrientation);
-	
+
+	// GALERIJA
+	$("#btnOpenGalery").click(function() {
+
+		navigator.camera.getPicture(onSuccess, onFail, {
+			quality : 50,
+			destinationType : navigator.camera.DestinationType.FILE_URI,
+			sourceType : navigator.camera.PictureSourceType.PHOTOLIBRARY
+		});
+
+	});
+
 }
 
+
+
 function onSuccess(imageURI) {
-    var image = document.getElementById('myImage');
-    image.src = imageURI;
-	
-	if ($(document).width() > $(document).height()){
+	$("#navigacija").hide();
+	var image = document.getElementById('myImage');
+	image.src = imageURI;
+
+	if ($(document).width() > $(document).height()) {
 		strWindowOrientation = "landscape";
-	}else{
-		strWindowOrientation = "portrait";		
+	} else {
+		strWindowOrientation = "portrait";
 	}
-	
-	if (image.width > image.height){
+
+	if (image.width > image.height) {
 		strImageOrientation = "landscape";
-	}else{
-		strImageOrientation = "portrait";		
+	} else {
+		strImageOrientation = "portrait";
 	}
-	
+
 	var intWidth = $(window).width();
 	var intHeight = $(window).height();
-	
-	if (strWindowOrientation != strImageOrientation){
+
+	if (strWindowOrientation != strImageOrientation) {
 		intWidth = $(window).height();
 		intHeight = $(window).width();
 	}
-		
-	$("#myImage").jqPuzzle({
-		window_width: intWidth,
-		window_height: intHeight
-	},{}, function(){ $("#container").css("opacity","1"); });
-	
-}
 
+	$("#myImage").jqPuzzle({
+		window_width : intWidth,
+		window_height : intHeight
+	}, {}, function() {
+		$("#container").css("opacity", "1");
+	});
+
+}
 
 function onFail(message) {
 	alert('Failed because: ' + message);
-}
-
-function onOrientationChange(){
-	alert("d");
-	
 }
