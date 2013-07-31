77fa7e69eb043b4512babce868c14fe14fabb3b6
s01oblak <sasoblak@gmail.com>
2013-07-31 15:52:47 +0200

    sdf

--- js/common.js
+++ js/common.js
@@ -1,7 +1,6 @@
 var strWindowOrientation = "";
 var strImageOrientation = "";
 
-
 // Wait for device API libraries to load
 //
 document.addEventListener("deviceready", onDeviceReady, false);
@@ -10,9 +9,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 //
 function onDeviceReady() {
 	$("#container").css("opacity", "0");
-	
-	
-
 
 	$(window).on("orientationchange", function(event) {
 		//alert("This device is in " + event.orientation + " mode!");
@@ -47,93 +43,81 @@ function onDeviceReady() {
 
 	});
 
-
 }
-
-
 
 function onSuccess(imageURI) {
 	$("#navigacija").hide();
 	var image = document.getElementById('myImage');
 	image.src = imageURI;
-alert("1");
+	alert("1");
 	var strWindowOrientation = dobiOrintacijoOkna();
 	var strImageOrientation = dobiOrintacijoSlike(image);
-	
+
 	var intWindowRazmerje = 0;
 	//alert(typeof intWindowRazmerje);
-	if ($(window).width() > $(window).height()){
+	if ($(window).width() > $(window).height()) {
 		intWindowRazmerje = $(window).height() / $(window).width();
-	}else{
+	} else {
 		intWindowRazmerje = $(window).width() / $(window).height();
 	}
-	
+
 	var intImgRazmerje = 0;
 	//alert(typeof intWindowRazmerje);
-	if (image.width > image.height){
+	if (image.width > image.height) {
 		intImgRazmerje = image.height / image.width;
-	}else{
+	} else {
 		intImgRazmerje = image.width / image.height;
 	}
 	alert("2");
-	
+
 	var intWidth = $(window).width();
 	var intHeight = $(window).height();
-		
+
 	var intWindowLong = 0;
 	var intWindowShort = 0;
-	if (intWidth > intHeight){
+	if (intWidth > intHeight) {
 		intWindowLong = intWidth;
 		intWindowShort = intHeight;
-	}else{
+	} else {
 		intWindowLong = intHeight;
 		intWindowShort = intWidth;
 	}
-	
+
 	alert("3");
-	
-	
-	
-	var intNewImgWidth = 0; 
+
+	var intNewImgWidth = 0;
 	var intNewImgHeight = 0;
-	if (strImageOrientation == "landscape"){
-		
-		if (intImgRazmerje < intWindowRazmerje){
+	if (strImageOrientation == "landscape") {
+
+		if (intImgRazmerje < intWindowRazmerje) {
 			alert("a");
 			// zelo podolgovati - prilagaja se daljsa stranica
 			intNewImgWidth = intWindowLong;
 			intNewImgHeight = Math.floor(intNewImgWidth * intImgRazmerje);
-		}else{
+		} else {
 			alert("b");
 			intNewImgHeight = intWindowShort;
 			intNewImgWidth = Math.floor(intNewImgHeight * (image.width / image.height));
 		}
-		
-		
-		
-	}else{
-		if (intImgRazmerje < intWindowRazmerje){
+
+	} else {
+		if (intImgRazmerje < intWindowRazmerje) {
 			alert("c");
 			// zelo podolgovati - prilagaja se daljsa stranica
 			intNewImgHeight = intWindowLong;
 			intNewImgWidth = Math.floor(intNewImgHeight * intImgRazmerje);
-			
-		}else{
+
+		} else {
 			alert("d");
 			intNewImgWidth = intWindowShort;
 			intNewImgHeight = Math.floor(intNewImgWidth * (image.height / image.width));
 		}
-			
-		
-		
-	}
-	
-	alert("4");
-	
-	
-	
-	
 
+	}
+
+	alert("4");
+
+/*
 	$("#myImage").jqPuzzle({
 		window_width : intNewImgWidth,
 		window_height : intNewImgHeight
@@ -141,26 +125,17 @@ alert("1");
 		alert("5");
 		$("#container").css("opacity", "1");
 	});
-
+*/
+	$("#myImage").jqPuzzle();
+	$("#container").css("opacity", "1");
+	
 }
-
-
-
-
-
-
-
-
-
-
-
 
 function onFail(message) {
 	alert('Failed because: ' + message);
 }
 
-
-function dobiOrintacijoOkna(){
+function dobiOrintacijoOkna() {
 	//alert($(document).width());
 	if ($(document).width() > $(document).height()) {
 		return "landscape";
@@ -169,7 +144,7 @@ function dobiOrintacijoOkna(){
 	}
 }
 
-function dobiOrintacijoSlike(image){
+function dobiOrintacijoSlike(image) {
 	if (image.width > image.height) {
 		return "landscape";
 	} else {
